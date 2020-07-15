4e5a7949c12f71af881c850cf69038b0976178e3
https://github.com/jorijnsmit/dsp.c4 <mideruit@gmail.com>
2020-07-16 01:13:08 +0200

    Added a ROUGE evaluation example.

--- Examples.py
+++ Examples.py
@@ -42,3 +42,24 @@ for sentence in summary_2:
 
 
 
+#%%
+### ROUGE Evaluation ###
+
+# %%
+
+hypothesis = "the #### transcript is a written version of each day 's cnn student news program use this transcript to he    lp students with reading comprehension and vocabulary use the weekly newsquiz to test your knowledge of storie s you     saw on cnn student news"
+
+reference = "this page includes the show transcript use the transcript to help students with reading comprehension and     vocabulary at the bottom of the page , comment for a chance to be mentioned on cnn student news . you must be a teac    her or a student age # # or older to request a mention on the cnn student news roll call . the weekly newsquiz tests     students ' knowledge of even ts in the news"
+
+rouge = Rouge()
+scores = rouge.get_scores(hypothesis, reference)
+
+# %%
+print(scores)
+print(scores[0])
+print(scores[0]['rouge-1'])
+print(scores[0]['rouge-2'])
+print(scores[0]['rouge-l'])
+print(scores[0]['rouge-1']['p'])
+print(scores[0]['rouge-1']['r'])
+print(scores[0]['rouge-1']['f'])
